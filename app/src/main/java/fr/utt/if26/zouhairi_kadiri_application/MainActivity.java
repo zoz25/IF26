90ef0513ea4b17f575d2ba2dd9730cec332cd25b
zoz25 <chelseacity25@gmail.com>
2017-10-18 00:36:37 +0200

    Initial commit

---
+++ app/src/main/java/fr/utt/if26/zouhairi_kadiri_application/MainActivity.java
@@ -0,0 +1,15 @@
+package fr.utt.if26.zouhairi_kadiri_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Bienvenue sur notre application", Toast.LENGTH_SHORT).show();
+    }
+}
