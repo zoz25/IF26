74da20116668dc7d820c4712fadba7c7bede41bb
mouaouiya <mouaouiyaalkadiri@gmail.com>
2017-10-18 16:53:00 +0100

    test mouaouiyaaaaa

--- app/src/main/java/fr/utt/if26/zouhairi_kadiri_application/MainActivity.java
+++ app/src/main/java/fr/utt/if26/zouhairi_kadiri_application/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Bienvenue sur notre application mouaouiya zak", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Bienvenue sur notre application mouaouiya zak hhhhhhh", Toast.LENGTH_SHORT).show();
     }
 }
