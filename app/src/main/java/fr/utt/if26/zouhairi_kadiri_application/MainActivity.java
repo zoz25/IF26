6c92966115efce9f2ad4f69376b1eac212f29272
zoz25 <chelseacity25@gmail.com>
2017-10-18 15:52:00 +0200

    test zak

--- app/src/main/java/fr/utt/if26/zouhairi_kadiri_application/MainActivity.java
+++ app/src/main/java/fr/utt/if26/zouhairi_kadiri_application/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Bienvenue sur notre application mouaouiya", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Bienvenue sur notre application mouaouiya zak", Toast.LENGTH_SHORT).show();
     }
 }
